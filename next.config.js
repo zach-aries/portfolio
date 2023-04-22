1e5623b0975b1f3fee4d65623e0317ae45a0c875
Zach Aries <zach.j.aries@gmail.com>
2023-04-22 12:01:22 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export",
+  basePath: "/gh-pages-test",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
