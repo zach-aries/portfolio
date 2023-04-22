48e42cdd96678359f4376099c730387906b2efae
Zach Aries <zach.j.aries@gmail.com>
2023-04-22 12:03:21 +0100

    Deploy

---
+++ _next/static/AN723HU1sAQuNGVR5AMMX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/404349ff7d566e9b.css","static/chunks/pages/index-f0c24a5363392821.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
